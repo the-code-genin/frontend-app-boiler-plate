fc4b9eb4eaf90ccd22bc410251f8c1a98b9186b1
Iyiola-am <adekunle3317@gmail.com>
2021-02-05 12:02:52 +0100

    Implemented list app

---
+++ src/list-app.ts
@@ -0,0 +1,75 @@
+export default class ListApp {
+    private items: string[] = [];
+
+    constructor(readonly DOMElement: HTMLDivElement) {
+        let button = DOMElement.querySelector('button#add-button') as HTMLButtonElement;
+        button.addEventListener('click', () => {
+            this.addItem('');
+        });
+    }
+
+    public getItems(): string[] {
+        return this.items;
+    }
+
+    public getItem(index: number): string {
+        return this.items[index];
+    }
+
+    public addItem(item: string): number {
+        this.items.push(item);
+        this.renderList();
+        return this.items.length;
+    }
+
+    public updateItem(index: number, item: string): string {
+        this.items[index] = item;
+        this.renderList();
+        return item;
+    }
+
+    public removeItem(index: number): string {
+        let item = this.items.splice(index, 1);
+        this.renderList();
+        return item[0];
+    }
+
+    public createElement(index: number): HTMLDivElement {
+        let item = this.getItem(index);
+
+        // Create the container
+        let container = document.createElement('div');
+        container.style.display = 'flex';
+
+        // Input element
+        let content = document.createElement('input');
+        content.value = item;
+        content.addEventListener('input', (e) => {
+            this.items[index] = content.value;
+        });
+
+        // Button element
+        let button = document.createElement('button');
+        button.innerText = 'X';
+        button.addEventListener('click', (e) => {
+            this.removeItem(index);
+            this.renderList();
+        });
+
+        // Append elements
+        container.appendChild(content);
+        container.appendChild(button);
+
+        return container;
+    }
+
+    public renderList() {
+        let content = this.DOMElement.querySelector('div#content') as HTMLDivElement;
+        content.innerHTML = '';
+
+        for (let itemIndex in this.items) {
+            let el = this.createElement(Number(itemIndex));
+            content.appendChild(el);
+        }
+    }
+}
